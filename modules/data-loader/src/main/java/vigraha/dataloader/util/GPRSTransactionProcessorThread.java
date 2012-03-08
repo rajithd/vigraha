f6e890f2656615b2ac049f9194d98270e2064876
Naveen Dissanayake <caspertheghost1989@gmail.com>
2012-03-08 21:00:26 +0600

    gprs, lbs, sms, gprs transaction thread

---
+++ modules/data-loader/src/main/java/vigraha/dataloader/util/GPRSTransactionProcessorThread.java
@@ -0,0 +1,37 @@
+package vigraha.dataloader.util;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import vigraha.dataloader.repository.TransactionRepository;
+
+public class GPRSTransactionProcessorThread implements Runnable {
+
+    private static final String NEWLINE_CHARACTER = "\n";
+
+    private String record;
+    private String columnSeparator;
+    private TransactionRepository transactionRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(GPRSTransactionProcessorThread.class);
+
+    public GPRSTransactionProcessorThread(String record, String columnSeparator, TransactionRepository transactionRepository) {
+
+        if (record.endsWith(NEWLINE_CHARACTER)) {
+            record = record.substring(0, record.length() - 1);
+        }
+        this.record = record;
+        this.columnSeparator = columnSeparator;
+        this.transactionRepository = transactionRepository;
+
+    }
+
+    public void run() {
+        try {
+            transactionRepository.save(record, columnSeparator);
+        } catch (Exception e) {
+            //todo: Send SNMP trap
+            logger.error("Couldn't save the record ["+record+"] Unexpected exception thrown while saving the record",e);
+        }
+    }
+}
