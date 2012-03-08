3be39e70e48533c30927db46b6947821bcd5a3cf
Naveen Dissanayake <caspertheghost1989@gmail.com>
2012-03-08 20:22:49 +0600

    new table queries

--- db-setup/create_tables.sql
+++ db-setup/create_tables.sql
@@ -11,4 +11,53 @@ CREATE TABLE IF NOT EXISTS `voice_call` (
 `call_type` VARCHAR (100) NOT NULL,
 PRIMARY KEY (record_id)
 )
+
+CREATE TABLE IF NOT EXISTS `sms` (
+`record_id` INT NOT NULL ,
+`number_make_call` VARCHAR(11) NOT NULL ,
+`number_receive_call` VARCHAR (11) NOT NULL ,
+`call_start_time` VARCHAR (100) NOT NULL ,
+`duration` VARCHAR (100) NOT NULL ,
+`number_charged_call` VARCHAR (11) NOT NULL ,
+`call_result` VARCHAR (50) NOT NULL ,
+`call_type` VARCHAR (100) NOT NULL,
+PRIMARY KEY (record_id)
+)
+
+CREATE TABLE IF NOT EXISTS `gprs` (
+`record_id` INT NOT NULL ,
+`number_make_call` VARCHAR(11) NOT NULL ,
+`number_receive_call` VARCHAR (11) NOT NULL ,
+`call_start_time` VARCHAR (100) NOT NULL ,
+`duration` VARCHAR (100) NOT NULL ,
+`number_charged_call` VARCHAR (11) NOT NULL ,
+`call_result` VARCHAR (50) NOT NULL ,
+`call_type` VARCHAR (100) NOT NULL,
+PRIMARY KEY (record_id)
+)
+
+CREATE TABLE IF NOT EXISTS `lbs` (
+`record_id` INT NOT NULL ,
+`number_make_call` VARCHAR(11) NOT NULL ,
+`number_receive_call` VARCHAR (11) NOT NULL ,
+`call_start_time` VARCHAR (100) NOT NULL ,
+`duration` VARCHAR (100) NOT NULL ,
+`number_charged_call` VARCHAR (11) NOT NULL ,
+`call_result` VARCHAR (50) NOT NULL ,
+`call_type` VARCHAR (100) NOT NULL,
+PRIMARY KEY (record_id)
+)
+
+CREATE TABLE IF NOT EXISTS `ussd` (
+`record_id` INT NOT NULL ,
+`number_make_call` VARCHAR(11) NOT NULL ,
+`number_receive_call` VARCHAR (11) NOT NULL ,
+`call_start_time` VARCHAR (100) NOT NULL ,
+`duration` VARCHAR (100) NOT NULL ,
+`number_charged_call` VARCHAR (11) NOT NULL ,
+`call_result` VARCHAR (50) NOT NULL ,
+`call_type` VARCHAR (100) NOT NULL,
+PRIMARY KEY (record_id)
+)
+
 ENGINE= InnoDB;
