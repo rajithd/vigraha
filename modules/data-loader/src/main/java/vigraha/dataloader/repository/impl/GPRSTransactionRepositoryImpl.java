18b3a370de39d7bc129492a1a556a2103636f501
Naveen Dissanayake <caspertheghost1989@gmail.com>
2012-03-24 10:29:00 +0600

    new repo files according to db

--- modules/data-loader/src/main/java/vigraha/dataloader/repository/impl/GPRSTransactionRepositoryImpl.java
+++ modules/data-loader/src/main/java/vigraha/dataloader/repository/impl/GPRSTransactionRepositoryImpl.java
@@ -19,8 +19,8 @@ public class GPRSTransactionRepositoryImpl implements TransactionRepository {
 
     public void save(String record, String columnSeparator) throws SQLException {
         String[] columnValues = record.split(columnSeparator);
-        jdbcTemplate.update("insert into `gprs` values(?,?,?,?,?,?,?,?)" , columnValues[0],columnValues[1],
-                columnValues[2], columnValues[3], columnValues[4], columnValues[5], columnValues[6], columnValues[7]);
+        jdbcTemplate.update("insert into `gprs` values(?,?,?,?,?)" , columnValues[0],columnValues[1],
+                columnValues[2], columnValues[3], columnValues[4]);
     }
 
 }
--- modules/data-loader/src/main/java/vigraha/dataloader/repository/impl/LBSTransactionRepositoryImpl.java
+++ modules/data-loader/src/main/java/vigraha/dataloader/repository/impl/LBSTransactionRepositoryImpl.java
@@ -20,8 +20,8 @@ public class LBSTransactionRepositoryImpl implements TransactionRepository {
 
     public void save(String record, String columnSeparator) throws SQLException {
         String[] columnValues = record.split(columnSeparator);
-        jdbcTemplate.update("insert into `lbs` values(?,?,?,?,?,?,?,?)" , columnValues[0],columnValues[1],
-                columnValues[2], columnValues[3], columnValues[4], columnValues[5], columnValues[6], columnValues[7]);
+        jdbcTemplate.update("insert into `lbs` values(?,?,?,?,?)" , columnValues[0],columnValues[1],
+                columnValues[2], columnValues[3], columnValues[4]);
     }
 
 }
