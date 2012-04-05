00f5f3c30b36b72715b732cb66ba5b91fa82c575
rajith delantha <rajithgama@gmail.com>
2012-04-05 16:36:39 +0530

    change rule engine print message

--- modules/rule-engine/src/main/java/vigraha/rule/boot/RuleEngineServer.java
+++ modules/rule-engine/src/main/java/vigraha/rule/boot/RuleEngineServer.java
@@ -3,6 +3,21 @@ package vigraha.rule.boot;
 public class RuleEngineServer {
 
     public static void main(String[] args) {
-        System.out.println("Rule Engine");
+        System.out.println("RRRRRRRRRRRRRRRRR                     lllllll                     \n" +
+                "R::::::::::::::::R                    l:::::l                     \n" +
+                "R::::::RRRRRR:::::R                   l:::::l                     \n" +
+                "RR:::::R     R:::::R                  l:::::l                     \n" +
+                "  R::::R     R:::::Ruuuuuu    uuuuuu   l::::l     eeeeeeeeeeee    \n" +
+                "  R::::R     R:::::Ru::::u    u::::u   l::::l   ee::::::::::::ee  \n" +
+                "  R::::RRRRRR:::::R u::::u    u::::u   l::::l  e::::::eeeee:::::ee\n" +
+                "  R:::::::::::::RR  u::::u    u::::u   l::::l e::::::e     e:::::e\n" +
+                "  R::::RRRRRR:::::R u::::u    u::::u   l::::l e:::::::eeeee::::::e\n" +
+                "  R::::R     R:::::Ru::::u    u::::u   l::::l e:::::::::::::::::e \n" +
+                "  R::::R     R:::::Ru::::u    u::::u   l::::l e::::::eeeeeeeeeee  \n" +
+                "  R::::R     R:::::Ru:::::uuuu:::::u   l::::l e:::::::e           \n" +
+                "RR:::::R     R:::::Ru:::::::::::::::uul::::::le::::::::e          \n" +
+                "R::::::R     R:::::R u:::::::::::::::ul::::::l e::::::::eeeeeeee  \n" +
+                "R::::::R     R:::::R  uu::::::::uu:::ul::::::l  ee:::::::::::::e  \n" +
+                "RRRRRRRR     RRRRRRR    uuuuuuuu  uuuullllllll    eeeeeeeeeeeeee  ");
     }
 }
