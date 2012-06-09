c415e8782e754b3009d5b8dcbadadd55a47dd2a9
unknown <Thejani@Thejani-PC.(none)>
2012-06-09 19:25:43 +0530

    forget pass word

---
+++ modules/admin-ui/src/main/java/vigraha/admin/controller/ForgetPasswordController.java
@@ -0,0 +1,25 @@
+package vigraha.admin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/forget-password")
+
+public class ForgetPasswordController {
+    private final static Logger logger = LoggerFactory.getLogger(ForgetPasswordController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String forgetPassword(Model model)
+    {
+        logger.info("==========Retrieving Forget password page============");
+        return "forget-password";
+    }
+
+}
+
