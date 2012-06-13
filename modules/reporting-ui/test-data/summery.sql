136ae20fae0c80ad560b5c705db513782e581e28
rajith delantha <rajithgama@gmail.com>
2012-06-13 23:21:32 +0530

    create promotion summery report page

---
+++ modules/reporting-ui/test-data/summery.sql
@@ -0,0 +1,2 @@
+insert into report_promotion_db_summery(promotion_rule_id,rule_id,company_code,company_name,programme_owner,promotion_name,start_date
+,end_date,promotion_number1,sms_message,execution_type,execution_time,status,msisdn,based_on) values(1,1,'CC1','Pizza Hut','Admin','Pizza Promotion','2012-06-15 06:00','2012-06-16 10:00','0112729729','SMS message','SPECIFIC','2012-06-16 15:00','Success','0779545015','Voice call')
