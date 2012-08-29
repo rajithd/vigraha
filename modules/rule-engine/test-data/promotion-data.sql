636ca95a356d6e79cc33264db81c9d775948f5dd
Rajith <rajithgama@gmail.com>
2012-08-29 21:52:36 +0530

    fix bugs

--- modules/rule-engine/test-data/promotion-data.sql
+++ modules/rule-engine/test-data/promotion-data.sql
@@ -1,5 +1,5 @@
 insert into promotion(company_code,promotion_name,start_date_time,end_date_time,based_on,promotion_number,cycle_type,cycle_time,restriction,selection_type,selection_value,age_restriction,sms_message,program_owner,promotion_status)
-values('c001','test1','2012-06-07 06:00','2012-07-22 14:00','SMS','0112729729','PROMOTION_END','2012-07-22 14:30','REPEAT','RANDOM','ALL','15,30','message','admin','CREATE');
+values('c001','test1','2012-06-07 06:00','2012-08-27 14:00','SMS','0112729729','PROMOTION_END','2012-07-29 20:37','REPEAT','RANDOM','ALL','15,30','message','admin','CREATE');
 
 insert into promotion(company_code,promotion_name,start_date_time,end_date_time,based_on,promotion_number,cycle_type,cycle_time,restriction,selection_type,selection_value,age_restriction,sms_message,program_owner,promotion_status)
 values('c001','test1','2012-06-07 06:00','2012-07-09 06:00','SMS','0112729729','PROMOTION_END','2012-07-10 06:00','REPEAT','RANDOM','ALL','15,30','message','admin','CREATE');
